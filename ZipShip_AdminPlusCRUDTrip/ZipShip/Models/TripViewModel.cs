12f6fb04c81b92975226c0593509dc48257c629c
Junaid Ali <johhny424@gmail.com>
2018-12-02 14:42:20 +0500

    By Junaid

---
+++ ZipShip_AdminPlusCRUDTrip/ZipShip/Models/TripViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ZipShip.Models
+{
+    public class TripViewModel
+    {
+        public string filtertrip { get; set; }
+        public int Id { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+        [Display(Name = "Travelers Name")]
+        public string AddedBy { get; set; }
+        public DateTime AddedOn { get; set; }
+
+    }
+}
